c3794b7db9f71fca03bf1c8a5f4816f6d9fe70b2
Nataliia Tyshchenko <tysyatsky@gmail.com>
2023-04-17 09:19:57 +0300

    Correcting WorkAtomic

--- parallelComputing_Lab3/Algorithm/Routine/Setting/WorkAtomic.cs
+++ parallelComputing_Lab3/Algorithm/Routine/Setting/WorkAtomic.cs
@@ -7,14 +7,23 @@ namespace Utils.Routine.Setting
     {
         public void SetMin(int newMin)
         {
-            if (newMin < Results.min)
-            {
-                Interlocked.Exchange(ref Results.min, newMin);
-            }
+            InterlockedExchangeIfSmallerThan(ref Results.min, Results.min, newMin);
         }
         public void SetCount(int newCount)
         {
             Interlocked.Add(ref Results.count, newCount);
+        }
+
+        public static bool InterlockedExchangeIfSmallerThan(ref int location, int comparison, int newValue)
+        {
+            int initialValue;
+            do
+            {
+                initialValue = location;
+                if (initialValue > comparison) return false;
+            }
+            while (Interlocked.CompareExchange(ref location, newValue, initialValue) != initialValue);
+            return true;
         }
     }
 }
